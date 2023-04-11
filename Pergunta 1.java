2da05f369df0f62ea9bd7a2857ca9ea207c9c3a6
Thiago Rocha Gonçalves <111816471+thiaguera44@users.noreply.github.com>
2023-04-11 18:20:02 -0300

    Create Pergunta 1.java

---
+++ Pergunta 1.java
@@ -0,0 +1,23 @@
+
+package testetarget;
+
+public class TesteTarget {
+
+
+    public static void main(String[] args) {
+      
+      double indice = 13;
+      double soma = 0.0;
+      double k = 0.0;
+      
+      while (k < indice){
+      k += 1;
+      soma += k;             
+      }
+      
+        System.out.println(soma);
+      
+
+    }
+    
+}
